b0db24557a899a4cfefa55206a3ded1245cd9cb8
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2025-04-07 16:07:29 -0500

    Update usuario.model.ts

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,7 +1,9 @@
-export interface Usuario {
-    id: number;
-    nombre: string;
-    email: string;
-    password: string;
-    rol: string;
-  }
+export class Usuario {
+  constructor(
+    public id: number,
+    public nombre: string,
+    public email: string,
+    public password: string,
+    public rol: string
+  ) {}
+}
