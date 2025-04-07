567288f787107f5f8e7c79ec1a69436e2d3add66
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2025-04-07 15:47:08 -0500

    Update usuario.service.ts

--- src/app/core/services/usuario.service.ts
+++ src/app/core/services/usuario.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Usuario } from '../../models/usuario.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  constructor(private api: ApiService) { }
 
-  constructor() { }
+  getUsuarios(): Observable<Usuario[]> {
+    return this.api.get<Usuario[]>('usuarios'); 
+  }
+
+  // Obtener un usuario por su ID
+  getUsuario(id: number): Observable<Usuario> {  
+    return this.api.get<Usuario>(`usuarios/${id}`);  
+  }
+
+  crearUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.api.post<Usuario>('usuarios', usuario); 
+  }
+
+  eliminarUsuario(id: number): Observable<void> {
+    return this.api.delete<void>(`usuarios/${id}`); 
+  }
 }
