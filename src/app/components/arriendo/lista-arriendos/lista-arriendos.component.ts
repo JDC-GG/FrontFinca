cf84cfc91aef332f8e48db0c5570442a6c563722
JDC-GG <juan.cordoba0609@gmail.com>
2025-05-08 15:38:40 -0500

    rent lista

--- src/app/components/arriendo/lista-arriendos/lista-arriendos.component.ts
+++ src/app/components/arriendo/lista-arriendos/lista-arriendos.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-arriendos',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './lista-arriendos.component.html',
   styleUrls: ['./lista-arriendos.component.css']
 })
@@ -23,25 +26,25 @@ export class ListaArriendosComponent {
       precio: 4550,
       titulo: 'Faulkner Ave',
       direccion: '909 Woodland St, Michigan, IN',
-      imagen: 'src/assets/images/imagen3.jpg'
+      imagen: 'assets/images/imagen3.jpg'
     },
     {
       precio: 2400,
       titulo: 'St. Crystal',
       direccion: '2104 Foley St, Chicago, IL',
-      imagen: 'src/assets/images/imagen4.webp'
+      imagen: 'assets/images/imagen4.webp'
     },
     {
       precio: 1500,
       titulo: 'Cove Red',
       direccion: '7231 Riverland St, San Diego, CA',
-      imagen: 'assets/img5.jpg'
+      imagen: 'assets/images/imagen5.jpeg'
     },
     {
       precio: 1600,
       titulo: 'Tarpon Bay',
       direccion: '1253 Grand Lake Rd, Tampa, FL',
-      imagen: 'assets/img6.jpg'
+      imagen: 'assets/images/imagen6.jpg'
     }
   ];
   
