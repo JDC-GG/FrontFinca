0743cf53b85b12d0cfd16c8a65de708bff323a87
JDC-GG <juan.cordoba0609@gmail.com>
2025-05-08 15:15:20 -0500

    boton de crear propiedad

--- src/app/components/arriendo/lista-arriendos/lista-arriendos.component.ts
+++ src/app/components/arriendo/lista-arriendos/lista-arriendos.component.ts
@@ -2,10 +2,48 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-arriendos',
-  imports: [],
   templateUrl: './lista-arriendos.component.html',
-  styleUrl: './lista-arriendos.component.css'
+  styleUrls: ['./lista-arriendos.component.css']
 })
 export class ListaArriendosComponent {
-
+  propiedades = [
+    {
+      precio: 2015,
+      titulo: 'Palm Harbor',
+      direccion: '2699 Green Valley, Highland Lake, FL',
+      imagen: 'assets/images/imagen1.jpg'
+    },
+    {
+      precio: 2700,
+      titulo: 'Beverly Springfield',
+      direccion: '2821 Lake Sevilla, Palm Harbor, TX',
+      imagen: 'assets/images/imagen2.jpg'
+    },
+    {
+      precio: 4550,
+      titulo: 'Faulkner Ave',
+      direccion: '909 Woodland St, Michigan, IN',
+      imagen: 'src/assets/images/imagen3.jpg'
+    },
+    {
+      precio: 2400,
+      titulo: 'St. Crystal',
+      direccion: '2104 Foley St, Chicago, IL',
+      imagen: 'src/assets/images/imagen4.webp'
+    },
+    {
+      precio: 1500,
+      titulo: 'Cove Red',
+      direccion: '7231 Riverland St, San Diego, CA',
+      imagen: 'assets/img5.jpg'
+    },
+    {
+      precio: 1600,
+      titulo: 'Tarpon Bay',
+      direccion: '1253 Grand Lake Rd, Tampa, FL',
+      imagen: 'assets/img6.jpg'
+    }
+  ];
+  
 }
+
